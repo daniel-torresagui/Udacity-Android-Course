53b77381f48300a21d9e6da99f3fecab13c23053
Daniel <daniel.torres.aguilar@alumnos.upm.es>
2015-11-15 23:12:12 +0100

    Commit de los archivos tras la segunda clase

--- UdacityCourse/app/src/main/java/app/example/daniel/udacitycourse/MainActivity.java
+++ UdacityCourse/app/src/main/java/app/example/daniel/udacitycourse/MainActivity.java
@@ -1,18 +1,9 @@
 package app.example.daniel.udacitycourse;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -50,33 +41,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            List<String> weatherForecast = new ArrayList<String>();
-            weatherForecast.add("Lunes 2 - Sunny");
-            weatherForecast.add("Martes 3 - Cloudy");
-            weatherForecast.add("Miercloes 4 - Nothing XD");
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_item_forecast_textview,
-                    R.id.list_item_forecast_textview,
-                    weatherForecast);
-
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            ListView list = (ListView) rootView.findViewById(R.id.listview_forecast);
-            list.setAdapter(adapter);
-
-            return rootView;
-        }
-    }
 }
